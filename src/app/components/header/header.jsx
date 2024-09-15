bad063d1bf69974fd54b6bdf9405e5b702a7a47d
MauricioStrang <strangmauricio@gmail.com>
2024-09-15 12:27:16 -0300

    added slider img and decided on the header

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -1,50 +1,44 @@
 import Image from "next/image";
 import styles from "./header.module.css" 
+import Link from "next/link";
 
 export default function Header() {
   return (
 
     <div className={styles.container}>
 
-        <div className={styles.brgMenuContainer}>
-            <Image 
-                className={styles.brgMenu} 
-                src={'/menu.png'} alt="menu button"
-                width={30} height={30} 
+
+        <div className={styles.logoContainer}>
+            <Image
+                className={styles.logo} 
+                src={'/logo1.png'} alt="search button"
+                width={300} height={60} 
             />
         </div>
 
-        <div className={styles.logoContainer}>
-            <h1 className={styles.logo}> ACADEMIA MONICA </h1>
+        <div className={styles.navLinks}>
+            <div className={styles.homeLink}>
+                <Link
+                    href={'/'}
+                >
+                    Inicio</Link>
+            </div>
+
+            <div className={styles.aboutLink}>
+                <Link
+                    href={'/about'}
+                >
+                    Sobre la Academia</Link>
+            </div>
+
+            <div className={styles.coursesLink}>
+                <Link
+                    href={'/courses'}
+                >
+                    Cursos</Link>
+            </div>
+
         </div>
-
-
-        <div className={styles.searchContainer}>
-            <div className={styles.dropdown}>
-                <select name="Categorías" id="cars">
-                    <option value="volvo">Todas las Categorías</option>
-                    <option value="volvo">Remeras</option>
-                    <option value="saab">Herramientas</option>
-                    <option value="opel">Sticker</option>
-                </select>
-            </div>
-
-            <div className={styles.searchBarContainer}>
-                <div className={styles.searchBar}>
-                    <p>¿Qué estás buscando?</p>
-                </div>
-            </div>
-
-
-            <div className={styles.searchBtnContainer}>
-                <Image
-                    className={styles.searchBtn} 
-                    src={'/search.png'} alt="search button"
-                    width={25} height={25} 
-                />
-            </div>
-        </div>
-        
 
 
         <div className={styles.userContainer}>
@@ -53,15 +47,7 @@ export default function Header() {
                 src={'/user.png'} alt="user button"
                 width={30} height={30} 
             />
-            <p>Login</p>
-        </div>
-
-        <div className={styles.cartContainer}>
-            <Image
-                className={styles.cartBtn}
-                src={'/cart.png'} alt="cart button"
-                width={30} height={30} 
-            />
+            <p>Ingresar</p>
         </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
-import styles from "./page.module.css";
+import Image from "next/image";
+import styles from "./home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-        
+
+
+        <div className={styles.sliderContainer}>
+            <Image src='/slider.jpg' alt='' fill className={styles.sliderImg}/>
+        </div>
+
+        <div className={styles.mainContent}>
+<h1>fuck you</h1>
+        </div>
     </div>
   );
 }
