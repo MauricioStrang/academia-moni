45fa9f11a08c630954b6f7d705c882345ff5a22b
MauricioStrang <strangmauricio@gmail.com>
2024-09-17 19:49:19 -0300

    added banner and adjusted header

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -3,53 +3,80 @@ import styles from "./header.module.css"
 import Link from "next/link";
 
 export default function Header() {
+
+
   return (
+
 
     <div className={styles.container}>
 
-
-        <div className={styles.logoContainer}>
-            <Image
-                className={styles.logo} 
-                src={'/logo1.png'} alt="search button"
-                width={300} height={60} 
-            />
-        </div>
-
-        <div className={styles.navLinks}>
-            <div className={styles.homeLink}>
-                <Link
-                    href={'/'}
-                >
-                    Inicio</Link>
-            </div>
-
-            <div className={styles.aboutLink}>
-                <Link
-                    href={'/about'}
-                >
-                    Sobre la Academia</Link>
-            </div>
-
-            <div className={styles.coursesLink}>
-                <Link
-                    href={'/courses'}
-                >
-                    Cursos</Link>
-            </div>
-
-        </div>
+        <div className={styles.main}>  
+            <div className={styles.banner}></div>
 
 
-        <div className={styles.userContainer}>
-            <Image 
-                className={styles.userBtn}
-                src={'/user.png'} alt="user button"
-                width={30} height={30} 
-            />
-            <p>Ingresar</p>
-        </div>
-    </div>
+            <div className={styles.navLinks}>
+
+                <div className={styles.logoContainer}>
+                    <Image
+                        className={styles.logo} 
+                        src={'/logo1.png'} alt="search button"
+                        width={300} height={60} 
+                    />
+                </div>
+
+                <div className={styles.homeLink}>
+                    <Link
+                        href={'/'}
+                    >
+                        Inicio</Link>
+                </div>
+
+                <div className={styles.aboutLink}>
+                    <Link
+                        href={'/about'}
+                    >
+                        Sobre la Academia</Link>
+                </div>
+
+                <div className={styles.contactLink}>
+                    <Link
+                        href={'/contact'}
+                    >
+                        Contactame</Link>
+                </div>
+
+                <div className={styles.noticeLink}>
+                    <Link
+                        href={'/notice'}
+                    >
+                        Noticias</Link>
+                </div>
+
+                <div className={styles.coursesLink}>
+                    <Link
+                        href={'/courses'}
+                    >
+                        Cursos</Link>
+                </div>
+                
+                <div className={styles.userLink}>
+
+                    <Image 
+                        className={styles.userBtn}
+                        src={'/user.png'} alt="user button"
+                        width={30} height={30} 
+                    />  
+                    <Link
+                        href={'/user'}
+                    >
+                        Ingresar</Link>
+                </div>
+                    </div>
+
+            
+            
+            </div>   
+        </div>  
   );
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,12 @@ import styles from "./home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-
-
         <div className={styles.sliderContainer}>
             <Image src='/slider.jpg' alt='' fill className={styles.sliderImg}/>
         </div>
 
         <div className={styles.mainContent}>
-<h1>fuck you</h1>
+          <h1>fuck you</h1>
         </div>
     </div>
   );
